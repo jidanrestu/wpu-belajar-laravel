29c5d7ada0b5c8e704fdcbca0ddbd963e974d1c1
Jidan Restu Kurniawan <jidanrk47@gmail.com>
2024-03-28 09:24:49 +0700

    Belajar Laravel 8 | Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'name' => 'Jidan Restu Kurniawan',
+        'email' => 'jidan@me.com',
+        'image' => 'gambar.png'
+    ]);
+});
+
+Route::get('/posts', function () {
+    return view('posts');
 });
