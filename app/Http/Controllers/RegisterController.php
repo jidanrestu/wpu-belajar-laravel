569aad13bc97afbaa6543fecb32751aee3aa5d28
Jidan Restu Kurniawan <jidanrk47@gmail.com>
2024-04-03 09:51:14 +0700

    Belajar Laravel 8 | User Registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -11,5 +12,17 @@ class RegisterController extends Controller
         return view('register.index', [
             'title' => 'Register',
         ]);
+    }
+    public function store()
+    {
+        $data = request()->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => ['required', 'email:dns', 'max:255', 'unique:users'],
+            'password' => 'required|min:5|max:255'
+        ]);
+        $data['password'] = bcrypt($data['password']);
+        User::create($data);
+        return redirect('/login')->with('success', 'Registration successfull. Please login!');
     }
 }
