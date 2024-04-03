097761d1f616d59c35540f7b331e484004e3d6af
Jidan Restu Kurniawan <jidanrk47@gmail.com>
2024-04-03 10:49:01 +0700

    Belajar Laravel 8 | User Login & Middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,4 +12,23 @@ class LoginController extends Controller
             'title' => 'Login',
         ]);
     }
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required',
+        ]);
+        if (auth()->attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+        return back()->with('loginError', 'Login Failed!');
+    }
+    public function logout(Request $request)
+    {
+        auth()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
 }
