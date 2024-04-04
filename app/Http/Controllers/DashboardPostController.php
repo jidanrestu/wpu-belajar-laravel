98ef447d09127f91909fc195f2e0caf5aabdeba9
Jidan Restu Kurniawan <jidanrk47@gmail.com>
2024-04-04 15:42:28 +0700

    Belajar Laravel 8 | Preview, Update & Delete Image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -133,6 +133,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if ($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
